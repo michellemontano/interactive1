8f1c102eb55e86ed2ac75a4736e2cec43ee982aa
Michelle Montano <m.montano@cca.edu>
2018-04-19 18:57:20 -0700

    wehhhh

---
+++ website/assets/js/final.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('#answer1').click(function(){
+        $('#responses').append('Why did you click on me?');
+    });
+  });
+
+  $(document).ready(function(){
+      $('#answer2').click(function(){
+          $('#responses').append('I am not sure why.');
+      });
+    });
